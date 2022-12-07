7d0e55390771a257c0eaf556900ffcd446a6a45c
Seu nome <seuemail@exemplo.br>
2022-12-07 12:04:34 -0300

    Realizado quarta parte de exercicios de fixação

---
+++ javaScript/exercicio-fixacao-04.js
@@ -0,0 +1,16 @@
+const currentHour = 12;
+let message;
+
+if (currentHour >= 22) {
+    console.log(message = 'Não deverámos comer nada, é hora de dormir')
+} else if (currentHour >= 18 && currentHour < 22) {
+    console.log(message = 'Rango da noite, vamos jantar :D');
+} else if (currentHour >= 14 && currentHour < 18) {
+    console.log(message = 'Vamos fazer um bolo pro café da tarde?');
+} else if (currentHour >= 11 && currentHour < 14) {
+    console.log(message = 'Hora do almoço!!!');
+} else {
+    console.log(message = 'Hmmm, cheiro de café recém-passado');
+}
+
+console.log(message);
