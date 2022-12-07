5b7d95bfe0f3f7a9f4007c88310703e41a95b97d
Seu nome <seuemail@exemplo.br>
2022-12-07 12:14:33 -0300

    Realizado quinta parte do exercicio de fixação

---
+++ javaScript/exercicio-fixacao-05.js
@@ -0,0 +1,9 @@
+let weekDay = "sabado";
+
+if (weekDay === 'segunda-feira' || weekDay === 'terca-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
+    console.log('Oba, mais um dia de aprendizado na Trybe >:D');
+} else if (weekDay === 'sabado') {
+    console.log('É DIA DE MALDADE!!!');
+} else {
+    console.log('FINALMENTE, descanso merecido UwU');
+}
