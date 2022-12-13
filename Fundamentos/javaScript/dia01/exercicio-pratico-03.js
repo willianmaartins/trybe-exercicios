ec2889704278ef0b78fceeb9a418457ef6604cdb
Seu nome <seuemail@exemplo.br>
2022-12-13 19:06:06 -0300

    Realizado terceiro exercicio pratico

---
+++ Fundamentos/javaScript/dia01/exercicio-pratico-03.js
@@ -0,0 +1,11 @@
+const num1 = 105;
+const num2 = 95;
+const num3 = 15;
+
+if (num1 > num2 && num1 > num3) {
+    console.log(num1 + ' é o maior!');
+} else if (num2 > num1 && num2 > num3) {
+    console.log(num2 + ' é o maior!');
+} else {
+    console.log(num3 + ' é o maior!');
+}
