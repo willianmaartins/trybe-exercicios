fc8552eb234103bca53732da9fe5d320b98d998d
Seu nome <willian.maartins@hotmail.com>
2023-03-18 21:36:35 -0300

    feito exercicio com auxilio do gabarito

--- Front-End/Introdução-ao-Frontend-e-JavaScript-Assíncrono/dia-02/Pets/scr/main.js
+++ Front-End/Introdução-ao-Frontend-e-JavaScript-Assíncrono/dia-02/Pets/scr/main.js
@@ -1,1 +1,36 @@
 import './style.css';
+
+const getDogButton = document.querySelector('#random-dog');
+const getCatButton = document.querySelector('#random-cat');
+const surpriseMeButton = document.querySelector('#surprise-me');
+const petImage = document.querySelector('#random-pet-image');
+
+getDogButton.addEventListener('click', () => {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((data) => {
+      const petURL = data.message;
+      petImage.src = petURL;
+    })
+});
+
+getCatButton.addEventListener('click', () => {
+  fetch("https://aws.random.cat/meow")
+    .then((response) => response.json())
+    .then((data) => {
+      const petURL = data.file;
+      petImage.src = petURL;
+    })
+});
+
+surpriseMeButton.addEventListener('click', () => {
+  Promise.any([
+    fetch("https://aws.random.cat/meow"),
+    fetch("https://dog.ceo/api/breeds/image/random"),
+  ])
+  .then((response) => response.json())
+  .then((data) => {
+    const petURL = data.file || data.message;
+    petImage.src = petURL;
+  })
+});
